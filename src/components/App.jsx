121e1aa4672b809f0bee5b08de39ecc3388d7f96
Sumit Lal <sumitlal.techdev@gmail.com>
2020-04-06 21:36:02 -0700

    Was able to pull data from example data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import VideoList from './VideoList.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -9,9 +10,7 @@ var App = () => (
       <div className="col-md-7">
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
+      <VideoList />
     </div>
   </div>
 );
